e8480e1bdec889e346acc90c5e49c7432100e84e
Wu <samuel29466751@gmail.com>
2026-04-07 10:12:45 +0800

    First commit

---
+++ Assets/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Product : MonoBehaviour {
+
+    public int ProductID;
+    public string ProductName;
+    public string ProductType;
+    public int ProductSize;
+    public string ProductWorkmanship;
+}
